89c7b7d53cde99d418ee2037df6837a778c47df4
felich <felichpehagasaginting@gmail.com>
2026-08-17 03:09:55 +0700

    fix(mobile): explicitly define compileSdk 35 and buildToolsVersion 35.0.0

--- apps/mobile/android/app/build.gradle.kts
+++ apps/mobile/android/app/build.gradle.kts
@@ -6,7 +6,8 @@ plugins {
 
 android {
     namespace = "id.ac.cwe.sawitgo.mobile"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 35
+    buildToolsVersion = "35.0.0"
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -15,12 +16,9 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "id.ac.cwe.sawitgo.mobile"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 24
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
